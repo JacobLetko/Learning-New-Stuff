39d86249c32831c8385c2c9dd3a1a02e2e5e8e97
Jacob Letko <s179061@students.aie.edu.au>
2018-03-28 12:03:18 -0700

    bullets disapear on collision

---
+++ Assets/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        var hitplayer = hit.GetComponent<PlayerMove>();
+        if (hitplayer != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
