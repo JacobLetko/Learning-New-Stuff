8e2e98d77496889686c5f1b573289d8f278fe6f1
Jacob Letko <s179061@students.aie.edu.au>
2018-03-28 11:35:17 -0700

    initial commit

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour
+{
+    public GameObject bulletpref;
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        var x = Input.GetAxis("Horizontal") * 0.1f;
+        var z = Input.GetAxis("Vertical") * 0.1f;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            Fire();
+
+        transform.Translate(x, 0, z);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    void Fire()
+    {
+        // creates bullet
+        var bullet = (GameObject)Instantiate(bulletpref, transform.position - transform.forward, Quaternion.identity);
+
+        //make bullet move
+        bullet.GetComponent<Rigidbody>().velocity = -transform.forward * 4;
+
+        //
+        Destroy(bullet, 2f);
+    }
+}
