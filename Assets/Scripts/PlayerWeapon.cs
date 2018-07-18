1c9515e36f77b3b5ad3e80455184f971dba93734
Jacob Letko <s179061@students.aie.edu.au>
2018-07-18 14:18:15 -0700

    stuff

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -7,7 +7,7 @@ public class PlayerWeapon{
 
     public string name = "Glock";
 
-    public float damage = 10;
-    public float range = 200;
+    public float damage = 10f;
+    public float range = 100f;
 
 }
